3f761240c0784d33fe507101f8bf80fb3b1cf5c1
Roman Dziuba <dzyuba.roman09@gmail.com>
2024-02-14 19:31:52 +0200

    added reusable hero bg component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import Link from "next/link";
+import homeImg from '../../public/home.jpg';
+import Hero from '@/components/hero';
 
 export default function Home() {
   return (
-    <div>Home Page</div>
+    <Hero imgData={homeImg} imgAlt="car factory" title="Professional Cloud Hosting"/>
   );
 }
