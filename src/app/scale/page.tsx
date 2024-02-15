f35580048bafe1f58878455080147ad35e96d27f
Roman Dziuba <dzyuba.roman09@gmail.com>
2024-02-15 11:45:54 +0200

    added header component; styling

--- src/app/scale/page.tsx
+++ src/app/scale/page.tsx
@@ -1,3 +1,12 @@
-export default function ScalePage() {
-  return <div>Scale Page</div>
+import scaleImg from '../../../public/scale.jpg';
+import Hero from '@/components/hero';
+
+export default function scalePage() {
+  return (
+    <Hero
+      imgData={scaleImg}
+      imgAlt='steel factory'
+      title='Scale your app to infinity'
+    />
+  )
 }
